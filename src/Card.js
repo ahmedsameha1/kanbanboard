fdcafda15625485d88fead5eb5e961f30afce562
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2018-09-02 15:25:47 +0200

    Use marked to convert markdown to html

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CheckList from "./CheckList";
 import PropTypes from "prop-types";
+import marked from "marked";
 
 class Card extends Component {
     constructor() {
@@ -19,7 +20,7 @@ class Card extends Component {
         if ( this.state.showDetails) {
             cardDetails = (
             <div className="card__details">
-            {this.props.description}
+            <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
             <CheckList cardId={this.props.id} tasks={this.props.tasks} />
             </div>
             );
