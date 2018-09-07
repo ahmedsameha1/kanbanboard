6f01097b4b78f945199b0e4961a7dd7d4a30ae8f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2018-09-07 20:55:28 +0200

    Write the toggleTask method code

--- src/KanbanBoardContainer.js
+++ src/KanbanBoardContainer.js
@@ -32,7 +32,7 @@ class KanbanBoardContainer extends Component {
 
     deleteTask(cardId, taskId, taskIndex) {
         // Find the index of the card
-        let cardIndex = this.state.cards.findIndex(card => card.id == cardId);
+        let cardIndex = this.state.cards.findIndex(card => card.id === cardId);
 
         // Create a new object without the task
         let nextState = update(this.state.cards, {
@@ -52,6 +52,32 @@ class KanbanBoardContainer extends Component {
     }
 
     toggleTask(cardId, taskId, taskIndex) {
+        // Find the index of the card
+        let cardIndex = this.state.cards.findIndex(card => card.id === cardId);
+        // Save a reference to the task's "done" value
+        let newDoneValue;
+        // Using the $apply command, you will change the done value to its opposite
+        let nextState = update(this.state.cards, {
+            [cardIndex]: {
+                tasks: {
+                    [taskIndex]: {
+                        done: { $apply: (done) => {
+                        newDoneValue = !done;
+                        return newDoneValue;
+                    }
+                    }
+                }
+            }}
+    });
+    // Set the component state to the mutated object
+    this.setState({cards: nextState});
+
+    // Call the API to toggle the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+        method: "put",
+        headers: API_HEADERS,
+        body: JSON.stringify({done: newDoneValue})
+    });
     }
 
     render() {
